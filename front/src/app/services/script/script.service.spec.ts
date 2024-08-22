b0fe448e80e1df44268f6deb537a3d2146b92059
ANTHONY PACHAY <57168753+ANTHONYPACHAY@users.noreply.github.com>
2024-08-22 08:39:18 -0500

    cambios

--- front/src/app/services/script/script.service.spec.ts
+++ front/src/app/services/script/script.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ScriptService } from './script.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ScriptService', () => {
   let service: ScriptService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [ScriptService]
+    });
     service = TestBed.inject(ScriptService);
   });
 
